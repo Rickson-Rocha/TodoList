f40229963c5e2a047e8da3241d1788a85eadbe65
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-10-31 22:40:51 -0300

    migrando tabela de tarefas

---
+++ src/main/resources/db/migration/V2__create_table_task.sql
@@ -0,0 +1,10 @@
+create table tarefa (
+  task_id int auto_increment primary key,
+  task_title varchar(100),
+  task_startAt datetime,
+  task_endAt datetime,
+  task_done boolean,
+  task_priority varchar(50),
+  user_id int not null,
+  FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
