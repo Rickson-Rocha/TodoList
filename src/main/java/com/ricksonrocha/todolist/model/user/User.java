998c10cfe480ea44d088e63145070a089f7894d0
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-10-31 22:08:52 -0300

    criando model de usuário

---
+++ src/main/java/com/ricksonrocha/todolist/model/user/User.java
@@ -0,0 +1,37 @@
+package com.ricksonrocha.todolist.model.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name="user")
+@Table(name="user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+    
+    @NotBlank
+    private String password;
+
+    //private<Task> tasks = new ArrayList<Task>();
+
+
+}
