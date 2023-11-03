cb9a4c74e6f855bc9c5d8a4a32005b9e78a24080
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-11-03 08:17:05 -0300

    refatorando user model

--- src/main/java/com/ricksonrocha/todolist/model/user/User.java
+++ src/main/java/com/ricksonrocha/todolist/model/user/User.java
@@ -4,6 +4,8 @@ import com.ricksonrocha.todolist.model.task.Task;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,6 +13,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -24,17 +28,21 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class User {
+public class User  {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
-    private String Username;
+    @NotEmpty
+    @Column(name = "user_name", length = 100, nullable = false, unique = true)
+    @Size(min=5,max=100)
+    private String username;
     
-    @NotBlank
-    private String Userpassword;
+    @NotEmpty
+    @Column(name = "user_password", length = 60, nullable = false)
+    @Size(min=5,max=100)
+    private String userpassword;
     
     @OneToMany(mappedBy = "user")
     private List <Task> tasks = new ArrayList<Task>();
