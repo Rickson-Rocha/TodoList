fcc4cef0f0c92991a8f323e8d0889e732ff0c9b9
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-11-06 21:24:46 -0300

    atualiza modelo de Usuario

--- src/main/java/com/ricksonrocha/todolist/model/user/User.java
+++ src/main/java/com/ricksonrocha/todolist/model/user/User.java
@@ -1,7 +1,10 @@
 package com.ricksonrocha.todolist.model.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ricksonrocha.todolist.model.task.Task;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,10 +15,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,12 +24,9 @@ import lombok.Setter;
 
 @Entity(name="user")
 @Table(name="user")
-@Getter
-@Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class User  {
+public class User {
     
     @Id
     @Column(name="user_id")
@@ -43,10 +41,52 @@ public class User  {
     @NotEmpty
     @Column(name = "user_password", length = 60, nullable = false)
     @Size(min=5,max=100)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String userpassword;
     
     @OneToMany(mappedBy = "user")
     private List <Task> tasks = new ArrayList<Task>();
 
+    public User(Long id, String username, String userpassword) {
+        this.id = id;
+        this.username = username;
+        this.userpassword = userpassword;
+    }
 
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserpassword() {
+        return this.userpassword;
+    }
+
+    public void setUserpassword(String userpassword) {
+        this.userpassword = userpassword;
+    }
+
+    @JsonIgnore
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+    public void removeTask(Task task){
+        tasks.remove(task);
+    }
+    
 }
